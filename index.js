de69198767632d8bf53eb5ebd26f314fabb27f7b
vk <vkumar01.dev@gmail.com>
2023-07-07 15:02:09 +0530

    Implemented Chat History

--- index.js
+++ index.js
@@ -10,22 +10,25 @@ async function main() {
     console.log(colors.bold.green('Welcome to the Chatbot Programm!'));
     console.log(colors.bold.green('You can start chatting with the bot'));
 
+    // storing conversation history
+    const chatHistory = [];
 
     while (true) {
         const userInput = readLineSync.question(colors.yellow('You: '));
 
         try {
+            // Contructing messages by iterating over the history
+            const messages = chatHistory.map(([role, content]) => ({role, content}));
+
+            console.log(messages);
+            // add latest user input
+            messages.push({role: 'user', content: userInput});
+
+
             // call the API with user input
-
-
             const completion = await openai.createChatCompletion({
                 model: 'gpt-3.5-turbo',
-                messages: [
-                    {
-                        role: 'user',
-                        content: userInput
-                    }
-                ]
+                messages: messages
             });
 
             // displaying completion text/content
@@ -38,6 +41,12 @@ async function main() {
             }
 
             console.log(colors.green('Bot: ') + completionText);
+
+
+            // Updating chat history with input and response
+
+            chatHistory.push(['user', userInput]);
+            chatHistory.push(['assistant', completionText]);
 
         } 
         catch (error) {
